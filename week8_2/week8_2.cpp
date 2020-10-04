ad65e529834032b0d71f7dd92d209936f18c6c94
EKAPOP ONSAMANG <63011090@KMITL.AC.TH>
2020-10-04 14:12:44 +0700

    Add project files.

---
+++ week8_2/week8_2.cpp
@@ -0,0 +1,35 @@
+﻿#include<stdio.h>
+int main()
+{
+	char count[999];
+	scanf_s("%s", &count, 999);
+	int last = 0;
+	int check = 0;
+	for (int i = 0; i <= last; i++)
+	{
+		if (last == 0) //เช็คค่าสุดท้ายตอนเริ่มต้นเมื่อตัวสุดท้ายยังไม่โดนเปลี่ยนค่า
+		{
+			for (int i = 0; i < 999; i++)
+			{
+				if (count[i] == '\0')
+				{
+					last = i - 1;
+				}
+			}
+		}
+		if (count[i] != count[last])
+		{
+			check++;
+		}
+		last--;
+	}
+	if (check == 0)
+	{
+		printf("palindrome");
+	}
+	else
+	{
+		printf("not palindrome");
+	}
+	return 0;
+}
